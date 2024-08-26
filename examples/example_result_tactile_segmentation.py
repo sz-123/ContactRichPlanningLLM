41b9e190119b5ddcf71249f061c92bc3b193e98c
sz-123 <zheng.shen18@outlook.com>
2024-08-26 14:47:25 +0200

    Tactile segmentation.

---
+++ examples/example_result_tactile_segmentation.py
@@ -0,0 +1,11 @@
+{'label': 'idle', 'start timestamp': '16:59:05.782', 'end timestamp': '16:59:20.675'}
+{'label': 'compression', 'start timestamp': '16:59:20.856', 'end timestamp': '16:59:24.053'}
+{'label': 'linear_force', 'start timestamp': '16:59:24.239', 'end timestamp': '16:59:30.442'}
+{'label': 'rotational_force', 'start timestamp': '16:59:30.650', 'end timestamp': '16:59:48.784'}
+{'label': 'decompression', 'start timestamp': '16:59:49.014', 'end timestamp': '16:59:52.358'}
+{'label': 'idle', 'start timestamp': '16:59:52.596', 'end timestamp': '17:00:44.705'}
+{'label': 'compression', 'start timestamp': '17:00:45.011', 'end timestamp': '17:00:53.862'}
+{'label': 'linear_force', 'start timestamp': '17:00:54.185', 'end timestamp': '17:01:02.389'}
+{'label': 'rotational_force', 'start timestamp': '17:01:02.732', 'end timestamp': '17:01:19.200'}
+{'label': 'decompression', 'start timestamp': '17:01:19.546', 'end timestamp': '17:01:24.130'}
+{'label': 'idle', 'start timestamp': '17:01:24.469', 'end timestamp': '17:01:44.691'}
