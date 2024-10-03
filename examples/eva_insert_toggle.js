09300cb23e2cc6a470df36d4bd20453e303b7d75
Yue Zhang <yue22.zhang@tum.de>
2024-10-03 19:15:40 +0200

    Eva Transition

---
+++ examples/eva_insert_toggle.js
@@ -0,0 +1,9 @@
+function showBefore_insert_cable() {
+    document.getElementById("insert_w_ft_container").style.display = "block";
+    document.getElementById("insert_wo_ft_container").style.display = "none";
+}
+
+function showAfter_insert_cable() {
+    document.getElementById("insert_w_ft_container").style.display = "none";
+    document.getElementById("insert_wo_ft_container").style.display = "block";
+}
--- examples/tighten_wo_ft.js
+++ examples/tighten_wo_ft.js
@@ -1,11 +1,11 @@
 // Define the task description as a JavaScript string
-var stretch_wo_ft= `
-def is_stretched(force_threshold=5.0):
-    # post-condition to check if the object is stretched
-    resistance_force = perception.get_resistance_force()
-    return resistance_force > force_threshold
+var tighten_wo_ft= `
+def is_tightened(threshold=0.02):
+    # post-condition to check if the object is tightened
+    resistance_torque = perception.get_resistance_torque()
+    return resistance_torque > threshold
 `;
 
 // Insert the task description into the user-request-cap-container element
-document.getElementById('stretch_wo_ft').innerHTML = 
-'<pre style="background-color: #f8f9fa; overflow: visible; white-space: pre-wrap; word-wrap: break-word;" style="background-color: #f8f9fa; overflow: visible; white-space: pre-wrap; word-wrap: break-word;"><code class="language-python">' + Prism.highlight(stretch_wo_ft.trim(), Prism.languages.python, 'python') + '</code></pre>';
+document.getElementById('tighten_wo_ft').innerHTML = 
+'<pre style="background-color: #f8f9fa; overflow: visible; white-space: pre-wrap; word-wrap: break-word;" style="background-color: #f8f9fa; overflow: visible; white-space: pre-wrap; word-wrap: break-word;"><code class="language-python">' + Prism.highlight(tighten_wo_ft_wo_ft.trim(), Prism.languages.python, 'python') + '</code></pre>';
