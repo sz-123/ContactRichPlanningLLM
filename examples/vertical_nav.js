eb7bfdefad4fc6b0ca2cf38df1058e1c9108ec40
Yue Zhang <yue22.zhang@tum.de>
2024-10-04 16:58:59 +0200

    delete flex height

--- examples/vertical_nav.js
+++ examples/vertical_nav.js
@@ -1,12 +1,3 @@
-function toggleMenu(menuId) {
-    var menu = document.getElementById(menuId);
-    if (menu.classList.contains('show')) {
-        menu.classList.remove('show');
-    } else {
-        menu.classList.add('show');
-    }
-}
-
 document.querySelectorAll('.nav-link').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         if (this.getAttribute('href') !== "javascript:void(0);") {
